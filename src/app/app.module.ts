5ca3ffa966efaa144a2bfac6b84c4f227c02d09d
Marcin Stelmach <marcinxd4@gmail.com>
2017-11-20 23:55:15 +0100

    angular matierials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
-import { RepositoryComponent } from './components/repository/repository.component';
-import { VersionComponent } from './components/version/version.component';
-import { FileComponent } from './components/file/file.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/shared/header/header.component';
+import {RegisterComponent} from './components/register/register.component';
+import {LoginComponent} from './components/login/login.component';
+import {RepositoryComponent} from './components/repository/repository.component';
+import {VersionComponent} from './components/version/version.component';
+import {FileComponent} from './components/file/file.component';
 import {AppRoutingModule} from './app.routing.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {GlobalService} from './services/global.service';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -25,9 +28,12 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule
   ],
-  providers: [],
+  providers: [GlobalService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

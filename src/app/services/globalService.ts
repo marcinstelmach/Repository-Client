8493cb15ea85ab87c633b3c7c47a3d9b3e6ca2e9
Marcin Stelmach <marcinxd4@gmail.com>
2017-12-05 23:21:12 +0100

    confict fesolve

--- src/app/services/globalService.ts
+++ src/app/services/globalService.ts
@@ -2,5 +2,5 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class GlobalService {
-  public servicePath = 'http://localhost:44986/api/';
+  public servicePath = 'http://repositoryappapi20171115125709.azurewebsites.net/api//';
 }

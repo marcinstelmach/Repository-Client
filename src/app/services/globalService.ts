0fa4ad858a06596d0ce9e860b765ea318f305c64
Marcin Stelmach <marcinxd4@gmail.com>
2018-01-08 20:05:13 +0100

    pathFix

--- src/app/services/globalService.ts
+++ src/app/services/globalService.ts
@@ -2,5 +2,6 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class GlobalService {
+  // public servicePath = 'http://repositoryappapi20171115125709.azurewebsites.net/api/';
   public servicePath = 'http://localhost:44986/api/';
 }

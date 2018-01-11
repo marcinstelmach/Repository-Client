da3c0282341e663c41e52407ff2cde511ac247a7
Marcin Stelmach <mstelmach@pgs-soft.com>
2018-01-11 13:54:32 +0100

    Critical fix

--- src/app/services/globalService.ts
+++ src/app/services/globalService.ts
@@ -2,5 +2,6 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class GlobalService {
-  public servicePath = 'http://localhost:44986/api/';
+  // public servicePath = 'http://localhost:44986/api/';
+  public servicePath = 'http://marcinstelmach.azurewebsites.net/api/';
 }

2de15509b109a3e2098b9c010c55e29b96f7c2d5
Marcin Stelmach <mstelmach@pgs-soft.com>
2018-01-11 14:02:38 +0100

    conflict

--- src/app/services/globalService.ts
+++ src/app/services/globalService.ts
@@ -2,11 +2,6 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class GlobalService {
-<<<<<<< HEAD
-  // public servicePath = 'http://repositoryappapi20171115125709.azurewebsites.net/api/';
-  public servicePath = 'http://localhost:44986/api/';
-=======
   // public servicePath = 'http://localhost:44986/api/';
   public servicePath = 'http://marcinstelmach.azurewebsites.net/api/';
->>>>>>> develop
 }
